09d1ed5307ca6d9d7b0e9663d6aa9ba4703fb06e
SAHIL-studio <sahildhatunde1234@gmail.com>
2026-08-29 12:09:00 +0530

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -1,5 +1,4 @@
-from dotenv import load_dotenv
-load_dotenv()
+import streamlit as st
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -12,7 +11,8 @@ from langchain_core.output_parsers import StrOutputParser
 loader = PyPDFLoader('policies.pdf')
 pdf = loader.load()
 
-embedding = GoogleGenerativeAIEmbeddings(model = "gemini-embedding-001")
+embedding = GoogleGenerativeAIEmbeddings(model = "gemini-embedding-001",
+                                        google_api_key=st.secrets["google_api_key"])
 splitter = RecursiveCharacterTextSplitter(
     chunk_size= 800,
     chunk_overlap=150
